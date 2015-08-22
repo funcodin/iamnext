83af37771544422dd4fc386fc3cd65a450e4c588
Rohit Dhumal <Rohit@rohits-air.home>
2015-08-22 16:02:45 -0400

    Initial setup

---
+++ iamnext-core/src/main/java/com/multi/enterprise/iamnext/App.java
@@ -0,0 +1,13 @@
+package com.multi.enterprise.iamnext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
